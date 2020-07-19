6bc0ed8118878d3ec81e3af2b58b561f6a1194ca
jhorelrevilla <jhorel.revilla@ucsp.edu.pe>
2020-07-19 13:38:44 -0500

    Update bd_class.cpp

--- bd_class.cpp
+++ bd_class.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
 #include <sqlite3.h>
+#include <vector>
 using namespace std;
+vector<string> cola;
+
 
 int callback(void *NotUsed, int argc, char **argv, char **azColName){
-    int i;
-    for(i = 0; i<argc; i++){
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    for(int i = 0; i<argc; i++){
+        if(argv[i]){
+            string temp(argv[i]);
+            cola.push_back(temp);
+        }
     }
-    printf("\n");
     return 0;
 }
 class Base_datos{
@@ -25,48 +29,43 @@ public:
     string Consulta_directa(string sql){
         conexion=sqlite3_exec(database, sql.c_str(),callback,0,&zErrMsg);
     }
-    string Read(string tabla){
-        string sql="SELECT * FROM ";
-        sql+=tabla+";";
-        conexion=sqlite3_exec(database, sql.c_str(),callback,0,&zErrMsg);
-    }
-    string Create(string tabla,string at1,string at2,string at3=""){
-        string sql="INSERT INTO "+tabla;
-        switch (tabla[0]){
-        case 'N':
-            sql+=" (nombre,apellido,escuela) VALUES (";
-            sql+="'"+at1+"','"+at2+"','"+at3+"');";
-            conexion=sqlite3_exec(database, sql.c_str(),callback,0,&zErrMsg);
-            break;
-        case 'R':
-            sql+=" (id_nodo1,id_nodo2,id_atrib_relacion) VALUES (";
-            sql+=at1+", "+at2+", "+at3+");";
-            conexion=sqlite3_exec(database, sql.c_str(),callback,0,&zErrMsg);
-            break;
-        case 'A':
-            sql+=" (nombre_relacion,peso) VALUES (";
-            sql+="'"+at1+"','"+at2+"');";
-            conexion=sqlite3_exec(database, sql.c_str(),callback,0,&zErrMsg);
-            break;
-        default:
-            cout<<"Error Create"<<endl;
-            break;
-        }
-    }
     ~Base_datos(){
         sqlite3_close(database);
     }
 };
+string create_json(vector <string> c,vector<string> t){
+    string result="{";
+    auto temp=t.begin();
+    for(auto i:c){
+        if(*temp == t[t.size()-1]){
+            result=result+*temp+":'"+i+"',";
+            temp=t.begin();    
+            continue;
+        }
+        result=result+*temp+":'"+i+"',";
+        temp++;       
+    }
+    result[result.size()-1]='}';
+    return result;
+}
 int main(){    
-    //Crear Read
+    //ROWID es el id de cada elemento
     /*                        NODO
-    nombre string(20) | apellido string(20) | escuela string(20)
+    nombre string(20)
     /////////////////////////////////////////////////////////////
-                            Relacion
-    id_nodo1 int      | id_nodo2 int        | id_atrib_relacion int
-    /////////////////////////////////////////////////////////////*/
-   string sql="SELECT * FROM Nodo;";
-   Base_datos bd("Redes.db");
-   //bd.Consulta_directa(sql);
-   //bd.Create("Relacion","1","2","3");
+                            Relacion                          //relacion nodo  a nodo
+    id_1 int      | id_2 int   
+    /////////////////////////////////////////////////////////////
+                            Atributo  
+                           
+    nodo int          | nombre_dato string(20)      | dato string(20) //nodo es del nodo que pertenece ese atributo
+    /////////////////////////////////////////////////////////////
+    */
+   //string sql="insert into Relacion(id_1,id_2) values(1,2);";
+   vector<string> temp={"nombre_dato","dato"};
+   string sql="select nombre_dato,dato from Atributo;";
+   Base_datos bd("db1.db");
+   //bd.Read("Nodo");
+   bd.Consulta_directa(sql);
+   cout<<create_json(cola,temp);
 }
